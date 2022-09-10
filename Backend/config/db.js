fb48b117d24a063366fe3d054b503240bcb77e45
saladawid <dawidsalamon@gmail.com>
2022-09-10 10:40:38 +0200

    deployed test version

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import {config} from './config.js';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(config.database);
+        await mongoose.connect(config.database, {useNewUrlParser: true});
         console.log(`MongoDB Connected!: ${mongoose.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
